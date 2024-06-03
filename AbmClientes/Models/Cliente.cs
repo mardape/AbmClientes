c88610b29128117751899272d6f8e364b215a670
Marcos <mardaped1@gmail.com>
2024-06-02 23:05:41 -0300

    Add project files.

---
+++ AbmClientes/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbmClientes.Models
+{
+    public partial class Cliente
+    {
+        public int Dni { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Mail { get; set; }
+    }
+}
---
+++ AbmClientes/Models/abmclientesContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AbmClientes.Models
+{
+    public partial class abmclientesContext : DbContext
+    {
+        public abmclientesContext()
+        {
+        }
+
+        public abmclientesContext(DbContextOptions<abmclientesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            { /*
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=localhost; database=abmclientes; integrated security=true;");
+        */  } 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.HasKey(e => e.Dni)
+                    .HasName("PK__cliente__C0308574C17FFBD2");
+
+                entity.ToTable("cliente");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mail)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
